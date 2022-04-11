39d02357eba8cbf43e0ee9ab5434f784805facf2
notecolt <d27thColt@gmail.com>
2022-04-11 15:12:20 +0800

    Added Owner, Added Cat Attack, edited test world

---
+++ TT MO GREG/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerAttack : MonoBehaviour {
+    // Private Variables
+    [SerializeField] private GameObject _playerAttackObj;
+    private bool _isAttacking = false;
+    private float _attackTimer = 0.0f;
+
+    // Public Variables
+    public float attackTime = 0.3f;
+    public float attackDistance = 0.6f;
+    void Start() {
+        if (_playerAttackObj == null)
+            Debug.LogError("No Player Attack Child Object assigned to this field!", this);
+        
+        SetAttackState();
+    }
+
+    void Update() {
+        SetPlayerAttackRotation();
+        AttackInput();
+    }
+
+    // Gets input for Player Attack Object and sets an appropriate rotation + position relative to the player
+    private void SetPlayerAttackRotation() {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        double angleRad = Math.Atan2(input.y, input.x);
+        float angleDeg = (float)(angleRad * 180 / Math.PI);
+        
+        // Sets the position & angle of the attack relative to the player
+        _playerAttackObj.transform.rotation = Quaternion.Euler(-30, 0, angleDeg);
+        _playerAttackObj.transform.localPosition = attackDistance * (new Vector3((float)Math.Cos(angleRad), (float)Math.Sin(angleRad), 0));
+    }
+
+    // For Detecting Attack Input
+    private void AttackInput() {
+        if (Input.GetKeyDown(KeyCode.J) && !_isAttacking) {
+            SetAttackState(true);
+        }
+
+        if (_isAttacking) {_attackTimer += Time.deltaTime; Debug.Log(_attackTimer);}
+
+        if (_attackTimer >= attackTime && _isAttacking) {
+            SetAttackState();
+        }
+    }
+
+    // Either Resets or Sets the attack state and any related variables. False by default.
+    private void SetAttackState(bool state = false) {
+        _isAttacking = state;
+        _playerAttackObj.SetActive(state);
+        if (!state) _attackTimer = 0.0f;
+    }
+}
---
+++ TT MO GREG/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    // [Code from Help Me Find My Doll! lol]
+
+    // Private Variables
+    private Rigidbody2D _rb;
+    private Vector2 _input;
+
+    // Public Variables
+    public float playerMoveSpeed = 5.0f;
+    void Start() {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        _input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        _input = Vector2.ClampMagnitude(_input, 1f);
+    }
+    void FixedUpdate() {
+        _rb.MovePosition((Vector2)transform.position + _input * Time.deltaTime * playerMoveSpeed);
+    }
+}
